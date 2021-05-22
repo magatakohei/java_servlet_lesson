0a6a98d659e0b7ba3c2e0986ab1ed1a0961cf140
magako <qutianh@gmail.com>
2021-05-22 22:37:21 +0900

    add ListenerSample.java

--- src/main/java/servlet/CounterServlet.java
+++ src/main/java/servlet/CounterServlet.java
@@ -3,7 +3,6 @@ package servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,17 +14,17 @@ import javax.servlet.http.HttpServletResponse;
 public class CounterServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	public void init(ServletConfig config) throws ServletException {
-		super.init(config);
-
-		//訪問回数を表すIntegerインスタンスを新規作成し
-		//アプリケーションスコープに保存
-		Integer count = 0;
-		ServletContext application = config.getServletContext();
-		application.setAttribute("count", count);
-
-		System.out.println("init()が実行されました");
-	}
+	//	public void init(ServletConfig config) throws ServletException {
+	//		super.init(config);
+	//
+	//		//訪問回数を表すIntegerインスタンスを新規作成し
+	//		//アプリケーションスコープに保存
+	//		Integer count = 0;
+	//		ServletContext application = config.getServletContext();
+	//		application.setAttribute("count", count);
+	//
+	//		System.out.println("init()が実行されました");
+	//	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
@@ -49,7 +48,7 @@ public class CounterServlet extends HttpServlet {
 		out.println("</html>");
 	}
 
-	public void destroy() {
-		System.out.println("destory()が実行されました");
-	}
+	//	public void destroy() {
+	//		System.out.println("destory()が実行されました");
+	//	}
 }
